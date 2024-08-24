40c29a042f3d6329c7b466c2fd092c7774327d1e
LuanTreuke <128757273+LuanTreuke@users.noreply.github.com>
2024-08-23 21:37:36 -0300

    Commit-Pessoa-e-iPessoa

---
+++ src/Entities/Pessoa.cs
@@ -0,0 +1,19 @@
+using Biblioteca.Interfaces;
+
+namespace Biblioteca.Entities
+{
+	public abstract class Pessoa : IPessoa
+	{
+		public string Nome { get; set; }
+		public string CPF  { get; set; }
+		
+		public Pessoa(string nome, string cpf)
+		{
+			Nome = nome;
+			CPF = cpf;
+		}
+		
+		public abstract void ObterIdentificação();
+	}
+}
+
---
+++ src/Interfaces/iPessoa.cs
@@ -0,0 +1,9 @@
+namespace Biblioteca.Interfaces
+{
+    public interface IPessoa
+    {
+        string Nome { get; set; }
+        string CPF { get; set; }
+        void ObterIdentificacao();
+    }
+}
