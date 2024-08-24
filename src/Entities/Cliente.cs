03284a898007f43da673965579fadb69b26a4e6b
LuanTreuke <128757273+LuanTreuke@users.noreply.github.com>
2024-08-23 22:11:08 -0300

    Create Cliente.cs

---
+++ src/Entities/Cliente.cs
@@ -0,0 +1,19 @@
+using Biblioteca.Interfaces;
+
+namespace Biblioteca.Entities
+{
+    public class Cliente : Pessoa
+    {
+        public string Endereco { get; set; }
+
+        public Cliente(string nome, string cpf, string endereco) : base(nome, cpf)
+        {
+            Endereco = endereco;
+        }
+
+        public override void ObterIdentificacao()
+        {
+            Console.WriteLine($"Cliente: {Nome}, CPF: {CPF}, Endereço: {Endereco}");
+        }
+    }
+}
