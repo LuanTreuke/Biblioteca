f69558a468760121a7ff3f813fe014c970d91d2d
LuanTreuke <128757273+LuanTreuke@users.noreply.github.com>
2024-08-23 22:00:41 -0300

    Commit-Creating-Funcionario

---
+++ src/Entities/Funcionario.cs
@@ -0,0 +1,19 @@
+using Biblioteca.Interfaces;
+
+namespace Biblioteca.Entities
+{
+    public class Funcionario : Pessoa
+    {
+        public string Cargo { get; set; }
+
+        public Funcionario(string nome, string cpf, string cargo) : base(nome, cpf)
+        {
+            Cargo = cargo;
+        }
+
+        public override void ObterIdentificacao()
+        {
+            Console.WriteLine($"Funcionario: {Nome}, CPF: {CPF}, Cargo: {Cargo}");
+        }
+    }
+}
